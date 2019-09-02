4170e7ad6de5a97e87602ecb70abadf50d1cf56a
matheusrmeloo <matheusrodrigues030717@gmail.com>
2019-09-02 19:32:07 -0300

    Adding Degub Function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const debug = require(`debug`)(`app:startup`);
 const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -18,7 +19,7 @@ console.log(`Password Server: ` + config.get(`mail.password`));
 
 if (app.get(`env`) === `development`){
     app.use(morgan('tiny'));
-    console.log(`Morgan enabled...`);
+    debug(`Morgan enabled...`);
 }
 
 
