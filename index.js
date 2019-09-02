a3d1f5cd983f1897237b5e2b0702a72328e12378
matheusrmeloo <matheusrodrigues030717@gmail.com>
2019-09-02 16:18:19 -0300

    Adicionando PUT e DELETE

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.json());
 
 const courses = [
-    {id:1,name:'course1'},
-    {id:2,name:'course2'},
-    {id:3,name:'course3'},
-    {id:4,name:'course4'}
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+    { id: 4, name: 'course4' }
 ]
 
 
@@ -16,37 +16,68 @@ app.get('/', (req, res) => {
     res.send('HelloWorld');
 });
 
-app.get('/api/courses', (req,res) => {
+app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
-app.get('/api/courses/:id', (req,res) => {
+app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('Id não encontrado');
+    if (!course) res.status(404).send('Id não encontrado');
     else res.send(course);
 });
 
-app.post('/api/courses', (req,res) => {
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    console.log(courses);
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.status(200).send({ course });
+});
+
+app.put(`/api/courses/:id`, (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Id não encontrado');
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+       return res.status(400).send(error.details[0].message);
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+    console.log(courses);
+
+});
+
+app.delete(`/api/courses/:id`, (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course){ 
+        return res.status(404).send('Id não encontrado');
+    };
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+    console.log(course);
+    console.log(courses);
+});
+
+function validateCourse(course) {
     const schema = {
         name: Joi.string().min(2).required()
     };
 
-    const result = Joi.validate(req.body, schema);
-    console.log(result);
+    return Joi.validate(course, schema);
 
-
-    if (result.error){
-        res.status(404).send(result.error);
-        return;
-    }
-    
-    const course = {
-        id: courses.length+1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-});
+}
 
 const port = process.env.PORT || 3000;
-app.listen(port,() => console.log('Listening on port ${port} ...'));
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
