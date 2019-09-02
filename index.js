5cdcb14a08d096ac65ff3feb7c955559129f15c0
matheusrmeloo <matheusrodrigues030717@gmail.com>
2019-09-02 20:35:08 -0300

    Install MongoDB

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const auth = require (`./auth`);
 const express = require('express');
 const app = express();
 
+app.set(`view engine`,`pug`);
+app.set(`views`, `./views`);
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(`public`));
@@ -36,7 +39,7 @@ const courses = [
 
 
 app.get('/', (req, res) => {
-    res.send('HelloWorld');
+    res.render(`index`, {title: `My Express App`, message:`ok`});
 });
 
 app.get('/api/courses', (req, res) => {
